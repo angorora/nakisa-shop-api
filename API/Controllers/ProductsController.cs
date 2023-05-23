af1b1384f73ec25c73d8d9137d2501cee4c05c72
Allen  Ngorora <allen.ngorora@gmail.com>
2023-05-23 21:58:43 +0200

    I am enjoying this, now reached a stable working state

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿
+
+using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _dbContext;
+        public ProductsController(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await  _dbContext.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int Id)
+        {
+            return await _dbContext.Products.FindAsync(Id);
+        }
+    }
+}
+
